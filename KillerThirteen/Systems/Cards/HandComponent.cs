d47891d5dde19c912f353477c76eec86570c7d61
SirkkasIdyll <sirkkas.idyll@protonmail.com>
2026-05-31 15:38:22 -0700

    Players will just have a HandComponent that tracks their cards

---
+++ KillerThirteen/Systems/Cards/HandComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Godot;
+using KillerThirteen.Temperance.NCS;
+
+namespace KillerThirteen.Systems.Cards;
+
+[GlobalClass]
+public partial class HandComponent : Component
+{
+    public List<Node<CardComponent>> Cards = [];
+}
